cc75634c60cce67ce769182bfd8b4b90685df25f
albre <amc2kmol@gmail.com>
2025-04-11 21:32:03 -0700

    fix: update memory endpoint and vercel config

--- api/memory.js
+++ api/memory.js
@@ -1,13 +1,12 @@
 export default async function handler(req, res) {
-    const response = await fetch("https://albre.xyz/memory", {
-      method: req.method,
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(req.body),
-    });
-  
-    const data = await response.json();
-    res.status(response.status).json(data);
-  }
-  
+  const response = await fetch("https://albre.xyz/memory", {
+    method: req.method,
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(req.body),
+  });
+
+  const data = await response.json();
+  res.status(response.status).json(data);
+}
